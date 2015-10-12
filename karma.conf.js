6383724c07aee2845fa0eac3dca92c75e07134c7
wokia <wokiatomosaka@gmail.com>
2015-10-13 03:35:23 +0900

    Karma の Reporter を変更.

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: process.env.TRAVIS ? ['junit'] : ['progress'],
+    reporters: process.env.TRAVIS ? ['mocha'] : ['mocha'],
 
     junitReporter: {
       outputDir: '',
