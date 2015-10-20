7b32702776c6d6e4f8445b43f03cdb60c0be70df
wokia <wokiatomosaka@gmail.com>
2015-10-21 00:11:04 +0900

    environment module は必要なくなったので削除.

--- source/utility/Environment.ts
+++
@@ -1,8 +0,0 @@
-module environment {
-
-	export function isLocalTesting():boolean {
-		// 全然正しい判定ではないけど、今の実用上の妥協点という事で…
-		const LOCALTESTING_LOCATION_ORIGIN = 'http://localhost:9876';
-		return (window.location.origin == LOCALTESTING_LOCATION_ORIGIN);
-	}
-}
--- spec/environment.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('utility/Environment', function() {
-	it('Local test environment can be determined correctly?', function() {
-		expect(environment.isLocalTesting()).toBeTruthy();
-	})
-})
